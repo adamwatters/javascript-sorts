58c41e459d7e4c36620551b84b060a669baeec79
HiroAgustin <hiroagustin@gmail.com>
2014-11-12 11:16:15 -0500

    bubble sort

---
+++ bubble-sort.js
@@ -0,0 +1,38 @@
+;(function ()
+{
+  'use strict';
+
+  var my_array = [5, 1, 7, 32, 8, 4, 0, 9, 11, 30];
+
+  function bubbleSort (collection)
+  {
+    var goAgain = false;
+    var index = 0;
+
+    while (index < collection.length - 1)
+    {
+      var first = collection[index];
+      var second = collection[index + 1];
+
+      if (second < first)
+      {
+        collection[index] = second;
+        collection[index + 1] = first;
+
+        goAgain = true;
+      }
+
+      index++;
+    }
+
+    if (goAgain)
+      bubbleSort(collection);
+
+    return collection;
+  }
+
+  bubbleSort(my_array);
+
+  console.log(my_array);
+
+}());
